b56e5657d836ea3e88a38112a7abfd3fd6c48772
Nicolas Hery <hery.nicolas@gmail.com>
2025-06-05 22:08:13 -0400

    Initial commit

---
+++ bean/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    char *name = "bean";
+    printf("Hello, %s!", name);
+}
---
+++ snake/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    char *name = "snake";
+    printf("Hello, %s!", name);
+}
