8cd47049739b36c6d981aa3fe7df9c91de81bb3b
Nicolas Hery <hery.nicolas@gmail.com>
2025-12-07 17:21:53 -0500

    Falling block

--- dodge/main.c
+++ dodge/main.c
@@ -6,6 +6,8 @@
 #define PLAYER_HEIGHT 60
 #define PLAYER_SPEED 240 // in pixels per second
 
+#define BLOCK_SIZE 50
+
 typedef enum {
     TITLE,
     GAMEPLAY,
@@ -16,12 +18,24 @@ typedef struct {
     Vector2 position;
 } Player;
 
-void game_state_reset(Player *player) {
+typedef struct {
+    Vector2 position;
+    float speed;
+} Block;
+
+void game_state_reset(Player *player, Block *block) {
     // Center at bottom of screen
     player->position = (Vector2){
         .x = (float)GetScreenWidth() / 2 - (float)PLAYER_WIDTH / 2,
         .y = (float)GetScreenHeight() - PLAYER_HEIGHT - 10,
     };
+
+    // Center at top of screen
+    block->position = (Vector2){
+        .x = (float)GetScreenWidth() / 2 - (float)BLOCK_SIZE / 2,
+        .y = 10,
+    };
+    block->speed = 300;
 }
 
 float clamp(float value, float min, float max) {
@@ -36,6 +50,24 @@ float clamp(float value, float min, float max) {
     return value;
 }
 
+Rectangle player_bounds(Player *player) {
+    return (Rectangle){
+        .x = player->position.x,
+        .y = player->position.y,
+        .width = PLAYER_WIDTH,
+        .height = PLAYER_HEIGHT,
+    };
+}
+
+Rectangle block_bounds(Block *block) {
+    return (Rectangle){
+        .x = block->position.x,
+        .y = block->position.y,
+        .width = BLOCK_SIZE,
+        .height = BLOCK_SIZE,
+    };
+}
+
 int main(void) {
     // Initialization
     //---------------------------------------------------------------------------------------------
@@ -47,7 +79,8 @@ int main(void) {
     GameScreen screen = TITLE;
 
     Player player = {0};
-    game_state_reset(&player);
+    Block block = {0};
+    game_state_reset(&player, &block);
 
     // Main game loop
     while (!WindowShouldClose()) {
@@ -75,11 +108,22 @@ int main(void) {
             if (IsKeyPressed(KEY_Q)) {
                 screen = ENDING;
             }
+
+            bool block_off_screen = (block.position.y > (float)GetScreenHeight());
+            if (block_off_screen) {
+                block.position.y = 10;
+            } else {
+                block.position.y += block.speed * GetFrameTime();
+            }
+
+            if (CheckCollisionRecs(player_bounds(&player), block_bounds(&block))) {
+                screen = ENDING;
+            }
         } break;
         case ENDING: {
             if (IsKeyPressed(KEY_ENTER)) {
                 screen = GAMEPLAY;
-                game_state_reset(&player);
+                game_state_reset(&player, &block);
             }
         } break;
         }
@@ -107,6 +151,8 @@ int main(void) {
                           PLAYER_WIDTH,
                           PLAYER_HEIGHT,
                           DARKBLUE);
+            DrawRectangle(
+                (int)block.position.x, (int)block.position.y, BLOCK_SIZE, BLOCK_SIZE, MAROON);
         } break;
         case ENDING: {
             DrawText("Game over!", 20, 20, 40, RED);
