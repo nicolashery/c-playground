933c1d98e5d47819f0e85c057c13da1bba31808b
Nicolas Hery <hery.nicolas@gmail.com>
2025-12-10 21:12:27 -0500

    Scoring and difficulty levels

--- dodge/main.c
+++ dodge/main.c
@@ -5,12 +5,21 @@
 
 #define PLAYER_WIDTH 100
 #define PLAYER_HEIGHT 60
-#define PLAYER_SPEED 240 // in pixels per second
+#define PLAYER_SPEED 240.0F // pixels per second
 
+#define MAX_BLOCKS 32
 #define BLOCK_SIZE 50
-#define BLOCK_SPEED 240
-#define MAX_BLOCKS 10
-#define SPAWN_INTERVAL 2.0F // seconds between spawns
+#define BLOCK_SPEED_INITIAL 120.0F         // pixels per second
+#define BLOCK_SPEED_LEVEL_INCREASE 0.1F    // percent increase per level
+#define SPAWN_INTERVAL_INITIAL 3.0F        // seconds between spawns
+#define SPAWN_INTERVAL_LEVEL_DECREASE 0.2F // percent decrease per level
+#define SCORE_POINTS_PER_BLOCK 50
+#define LEVEL_CHANGE_INTERVAL 5.0F // seconds of survival between levels
+#define MAX_LEVEL 20
+
+#define TEXT_BUFFER_LENGTH 1024
+
+#define DEBUG_INVINCIBLE false
 
 typedef enum {
     TITLE,
@@ -27,21 +36,41 @@ typedef struct {
     bool active;
 } Block;
 
-void game_state_reset(Player *player, Block *blocks, float *time_since_last_spawn) {
+typedef struct {
+    Player player;
+    Block blocks[MAX_BLOCKS];
+    float time_since_last_spawn;
+    float time_since_last_level;
+    int score;
+    int level;
+    float block_speed;
+    float spawn_interval;
+} GameState;
+
+void game_state_reset(GameState *state) {
     // Center at bottom of screen
-    player->position = (Vector2){
+    state->player.position = (Vector2){
         .x = (float)GetScreenWidth() / 2 - (float)PLAYER_WIDTH / 2,
         .y = (float)GetScreenHeight() - PLAYER_HEIGHT - 10,
     };
 
+    for (size_t i = 0; i < MAX_BLOCKS; i++) {
+        state->blocks[i].active = false;
+    }
     // Center at top of screen
-    blocks[0].position = (Vector2){
+    state->blocks[0].position = (Vector2){
         .x = (float)GetScreenWidth() / 2 - (float)BLOCK_SIZE / 2,
         .y = 10,
     };
-    blocks[0].active = true;
+    state->blocks[0].active = true;
 
-    *time_since_last_spawn = 0.0F;
+    state->time_since_last_spawn = 0.0F;
+    state->time_since_last_level = 0.0F;
+
+    state->score = 0;
+    state->level = 1;
+    state->block_speed = BLOCK_SPEED_INITIAL;
+    state->spawn_interval = SPAWN_INTERVAL_INITIAL;
 }
 
 float clamp(float value, float min, float max) {
@@ -82,12 +111,13 @@ int main(void) {
     SetTargetFPS(60);
 
     // Game state
-    GameScreen screen = GAMEPLAY;
+    GameScreen screen = TITLE;
+    GameState state = {0};
+    game_state_reset(&state);
 
-    Player player = {0};
-    Block blocks[MAX_BLOCKS] = {0};
-    float time_since_last_spawn = 0.0F;
-    game_state_reset(&player, blocks, &time_since_last_spawn);
+    // Text buffers
+    char score_text_buffer[TEXT_BUFFER_LENGTH] = {};
+    char level_text_buffer[TEXT_BUFFER_LENGTH] = {};
 
     // For debugging
     SetRandomSeed(123456);
@@ -104,16 +134,17 @@ int main(void) {
 
         } break;
         case GAMEPLAY: {
+            Player *player = &state.player;
             // Move player
             if (IsKeyDown(KEY_RIGHT)) {
-                player.position.x = clamp(player.position.x + (float)PLAYER_SPEED * GetFrameTime(),
-                                          0.0F,
-                                          (float)GetScreenWidth() - PLAYER_WIDTH);
+                player->position.x = clamp(player->position.x + PLAYER_SPEED * GetFrameTime(),
+                                           0.0F,
+                                           (float)GetScreenWidth() - PLAYER_WIDTH);
             }
             if (IsKeyDown(KEY_LEFT)) {
-                player.position.x = clamp(player.position.x - (float)PLAYER_SPEED * GetFrameTime(),
-                                          0.0F,
-                                          (float)GetScreenWidth() - PLAYER_WIDTH);
+                player->position.x = clamp(player->position.x - PLAYER_SPEED * GetFrameTime(),
+                                           0.0F,
+                                           (float)GetScreenWidth() - PLAYER_WIDTH);
             }
 
             // Quit gameplay
@@ -121,9 +152,9 @@ int main(void) {
                 screen = ENDING;
             }
 
-            // Move blocks
+            // Move blocks & check collision
             for (size_t i = 0; i < MAX_BLOCKS; i++) {
-                Block *block = &blocks[i];
+                Block *block = &state.blocks[i];
                 if (!block->active) {
                     continue;
                 }
@@ -131,27 +162,41 @@ int main(void) {
                 bool block_off_screen = (block->position.y > (float)GetScreenHeight());
                 if (block_off_screen) {
                     block->active = false;
+                    state.score += SCORE_POINTS_PER_BLOCK;
                 } else {
-                    block->position.y += BLOCK_SPEED * GetFrameTime();
+                    block->position.y += state.block_speed * GetFrameTime();
                 }
 
-                if (CheckCollisionRecs(player_bounds(&player), block_bounds(block))) {
-                    screen = ENDING;
+                if (!DEBUG_INVINCIBLE) {
+                    if (CheckCollisionRecs(player_bounds(player), block_bounds(block))) {
+                        screen = ENDING;
+                    }
                 }
             }
 
-            // Spawn blocks
-            time_since_last_spawn += GetFrameTime();
-            if (time_since_last_spawn > SPAWN_INTERVAL) {
-                time_since_last_spawn = 0.0F;
+            // Change level
+            state.time_since_last_level += GetFrameTime();
+            if (state.time_since_last_level > LEVEL_CHANGE_INTERVAL && state.level <= MAX_LEVEL) {
+                state.time_since_last_level = 0.0F;
+                state.level += 1;
 
+                state.block_speed = state.block_speed * (1.0F + BLOCK_SPEED_LEVEL_INCREASE);
+                state.spawn_interval =
+                    state.spawn_interval * (1.0F - SPAWN_INTERVAL_LEVEL_DECREASE);
+            }
+
+            // Spawn blocks
+            state.time_since_last_spawn += GetFrameTime();
+            if (state.time_since_last_spawn > state.spawn_interval) {
+                state.time_since_last_spawn = 0.0F;
+
+                // Find first available block in pool
                 Block *block = nullptr;
                 for (size_t i = 0; i < MAX_BLOCKS; i++) {
-                    if (blocks[i].active) {
-                        continue;
+                    if (!state.blocks[i].active) {
+                        block = &state.blocks[i];
+                        break;
                     }
-
-                    block = &blocks[i];
                 }
 
                 if (block == nullptr) {
@@ -170,7 +215,7 @@ int main(void) {
         case ENDING: {
             if (IsKeyPressed(KEY_ENTER)) {
                 screen = GAMEPLAY;
-                game_state_reset(&player, blocks, &time_since_last_spawn);
+                game_state_reset(&state);
             }
         } break;
         }
@@ -182,25 +227,30 @@ int main(void) {
 
         switch (screen) {
         case TITLE: {
-            DrawText("Dodge the Blocks", 20, 20, 40, DARKGREEN);
+            const char *title_text = "Dodge the Blocks";
+            DrawText(title_text,
+                     GetScreenWidth() / 2 - MeasureText(title_text, 40) / 2,
+                     GetScreenHeight() / 2 - 40,
+                     40,
+                     DARKGREEN);
             const char *start_text = "Press [ENTER] to start";
             DrawText(start_text,
                      GetScreenWidth() / 2 - MeasureText(start_text, 20) / 2,
-                     GetScreenHeight() / 2 + 10,
+                     GetScreenHeight() / 2 + 20,
                      20,
                      DARKGRAY);
 
         } break;
         case GAMEPLAY: {
-            DrawFPS(GetScreenWidth() - 80, 10);
-            DrawRectangle((int)player.position.x,
-                          (int)player.position.y,
+            Player *player = &state.player;
+            DrawRectangle((int)player->position.x,
+                          (int)player->position.y,
                           PLAYER_WIDTH,
                           PLAYER_HEIGHT,
                           DARKBLUE);
 
             for (size_t i = 0; i < MAX_BLOCKS; i++) {
-                Block *block = &blocks[i];
+                Block *block = &state.blocks[i];
                 if (!block->active) {
                     continue;
                 }
@@ -209,13 +259,31 @@ int main(void) {
                     (int)block->position.x, (int)block->position.y, BLOCK_SIZE, BLOCK_SIZE, MAROON);
             }
 
+            (void)snprintf(score_text_buffer, TEXT_BUFFER_LENGTH, "Score %d", state.score);
+            DrawText(score_text_buffer, 20, 20, 30, VIOLET);
+            (void)snprintf(
+                level_text_buffer, TEXT_BUFFER_LENGTH, "Level %02d/%d", state.level, MAX_LEVEL);
+            DrawText(level_text_buffer, 20, 60, 20, BLUE);
+
+            DrawFPS(GetScreenWidth() - 100, 20);
         } break;
         case ENDING: {
-            DrawText("Game over!", 20, 20, 40, RED);
+            (void)snprintf(score_text_buffer, TEXT_BUFFER_LENGTH, "Score %d", state.score);
+            DrawText(score_text_buffer, 20, 20, 30, VIOLET);
+            (void)snprintf(
+                level_text_buffer, TEXT_BUFFER_LENGTH, "Level %02d/%d", state.level, MAX_LEVEL);
+            DrawText(level_text_buffer, 20, 60, 20, BLUE);
+
+            const char *game_over_text = "Game over!";
+            DrawText(game_over_text,
+                     GetScreenWidth() / 2 - MeasureText(game_over_text, 40) / 2,
+                     GetScreenHeight() / 2 - 40,
+                     40,
+                     RED);
             const char *play_again_text = "Press [ENTER] to play again";
             DrawText(play_again_text,
                      GetScreenWidth() / 2 - MeasureText(play_again_text, 20) / 2,
-                     GetScreenHeight() / 2 + 10,
+                     GetScreenHeight() / 2 + 20,
                      20,
                      DARKGRAY);
         } break;
